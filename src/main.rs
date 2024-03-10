62a67a23515d92a2b635567896bcda406b121ff6
tomvdbroecke <tom@tvdb.me>
2024-03-10 20:47:38 +0100

    added more test endpoints

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,25 @@ async fn get_notes() -> impl Responder {
     HttpResponse::Ok().body("GET /notes")
 }
 
+#[post("/notes")]
+async fn create_notes() -> impl Responder {
+    HttpResponse::Ok().body("POST /notes")
+}
+
+#[patch("/notes")]
+async fn update_notes() -> impl Responder {
+    HttpResponse::Ok().body("PATCH /notes")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| actix_web::App::new().service(get_notes))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        actix_web::App::new()
+            .service(get_notes)
+            .service(create_notes)
+            .service(update_notes)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
