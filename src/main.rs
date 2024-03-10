8bf42e10fc4c167eeefa3739fd99c3d20c578150
tomvdbroecke <tom@tvdb.me>
2024-03-10 20:40:53 +0100

    starting the server, first test endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, patch, post, HttpResponse, HttpServer, Responder};
+
+#[get("/notes")]
+async fn get_notes() -> impl Responder {
+    HttpResponse::Ok().body("GET /notes")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| actix_web::App::new().service(get_notes))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
